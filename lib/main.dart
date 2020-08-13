7a26f2187072c995ce5cfa55cff4c5f161fad93c
Siamak Salimi <ssalimi1220@gmail.com>
2020-08-13 16:13:31 -0400

    functions updated: kelid: each note key

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,43 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void motreb(int note) {
+    final player = AudioCache();
+    player.play('note$note.wav');
+  }
+
+  Expanded kelid({Color color, int noteNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          motreb(noteNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(child: Text(nouns.first)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              kelid(color: Colors.red, noteNumber: 1),
+              kelid(color: Colors.yellow, noteNumber: 2),
+              kelid(color: Colors.orange, noteNumber: 3),
+              kelid(color: Colors.lightGreenAccent, noteNumber: 4),
+              kelid(color: Colors.blueAccent, noteNumber: 5),
+              kelid(color: Colors.purple, noteNumber: 6),
+              kelid(color: Colors.deepPurpleAccent, noteNumber: 7),
+            ],
+          ),
         ),
       ),
     );
